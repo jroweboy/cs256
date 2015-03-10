777b79aae4c2100a92d17d1acbcad253059658f7
James Rowe <jroweboy@gmail.com>
2015-03-09 23:22:55 -0600

    Update wireframe.js

--- wireframe/wireframe.js
+++ wireframe/wireframe.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var plus = elem.target;
         var newnode = document.createElement("li");
         newnode.innerHTML = "Recipe Name <span class=\"hide_if_multiple\">[x]</span></li>";
-        newnode.class = "tab";
+        newnode.className = "tab";
         plus.parentElement.insertBefore(newnode, plus);
+    }
+    document.querySelectorAll('#tabs>ul>li>span').onclick = function(elem) {
+        document.getElementById("#tabs").getElementsByTagName("ul")[0].removeNode(elem.target.parentElement);
     }
 });
